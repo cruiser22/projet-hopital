5604ea0096e70d0188aece251387e7c8122e6590
Florent DAGNOT <florent.dagnot@laposte.net>
2023-01-18 16:32:34 +0100

    Menus en place mais non-lies

--- src/user/Test.java
+++ src/user/Test.java
@@ -8,10 +8,10 @@ public class Test {
 		
 		System.out.println("Init main");
 
-		modeMenu();
+		metierMenu();
 	}
 
-	private static void modeMenu() {
+	private static void metierMenu() {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int choix = -1;
@@ -27,6 +27,7 @@ public class Test {
 				choix = sc.nextInt();
 			} catch (Exception e) {
 				System.out.println("Veuillez entrer un caratère numérique !");
+				break;
 			}
 			
 			if (choix == 0)
@@ -42,24 +43,141 @@ public class Test {
 	// menu authentification
 	private static void authMenu(int choix) {
 		Scanner sc = new Scanner(System.in);
-		//Authentification auth  = new Authentification();
-		String nom;
-		String motdepasse;
+		Authentification auth  = new Authentification();
+		String login;
+		String password;
 		int continuer = 0;
 		
 		try {
 			System.out.println("Veuillez saisir votre nom :");
-			nom = sc.nextLine();
+			login = sc.nextLine();
 			System.out.println("Veuillez saisir votre mot de passe :");
-			motdepasse = sc.nextLine();
+			password = sc.nextLine();
 		} catch (Exception e) {
 			System.out.println("Veuillez entrer des charactères ASCII !");
 		}
-		if (/*auth.verification(choix, nom, motdepasse)*/ true) {
+		if (auth.verification(choix, login, password)) {
 			System.out.println("Authentification a réussie !");
 			//Prochain menu
-		} else {
+			if (choix == 3)
+				menuSecretaire();
+			else
+				menuMedecin(choix);
+		} else
 			System.out.println("L'authentification a échouée.");
+	}
+	
+	private static void menuSecretaire() {
+		Secretaire s = new Secretaire();
+		Scanner sc = new Scanner(System.in);
+		int choix = -1;
+		
+		while (choix != 0) {
+			System.out.println("Veuillez choisir une action :");
+			System.out.println("1) Ajouter un patient en file d'attente\n"
+							 + "2) Afficher la file d'attente\n"
+							 + "3) Afficher le prochain patient\n"
+							 + "4) Afficher les visites d'un patient\n"
+							 + "0) Quitter le menu");
+			try {
+				choix = sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("Veuillez entrer un caratère numérique !");
+				break;
+			}
+			
+			switch(choix){
+			case 0 :
+				System.out.println("Retour au menu précédent.");
+				break;
+			case 1 :
+				s.addFileAttente(formulairePatient());
+				break;
+			case 2 :
+				s.afficherFileAttente();
+				break;
+			case 3 :
+				s.afficherNextPatient();
+				break;
+			case 4 :
+				s.afficherVisitesPatient();
+				break;
+			default :
+				System.out.println("Veuillez entrer un chiffre correspondant à votre choix !");
+				break;
+			}
+		}
+	}
+	
+	private static Patient formulairePatient() {
+		Scanner sc = new Scanner(System.in);
+		Patient p;
+		int id, age;
+		String nom, prenom, tel, adresse;
+		
+		do {
+			System.out.println("Veuillez renseigner les informations du patient :");
+			try {
+				System.out.println("Id");
+				id = sc.nextInt();
+				System.out.println("Nom");
+				nom = sc.nextLine();
+				System.out.println("Prenom");
+				prenom = sc.nextLine();
+				System.out.println("Age");
+				age = sc.nextInt();
+				System.out.println("Telephone");
+				tel = sc.nextLine();
+				System.out.println("Adresse");
+				adresse = sc.nextLine();
+				p = new Patient(id, nom, prenom, age, tel, adresse);
+			} catch (Exception e) {
+				System.out.println("Veuillez entrer un caratère numérique ou ASCII !");
+				break;
+			}
+		} while(p != null);
+		
+		return p;
+	}
+	
+	private static void menuMedecin(int salle) {
+		Medecin m = new Medecin();
+		Scanner sc = new Scanner(System.in);
+		int choix = -1;
+		
+		while (choix != 0) {
+			System.out.println("Veuillez choisir une action :");
+			System.out.println("1) Rendre la salle disponible\n"
+							 + "2) Afficher la file d'attente\n"
+							 + "3) Sauvegarder ses visites\n"
+							 + "4) Afficher sa liste des visites\n"
+							 + "0) Quitter le menu");
+			try {
+				choix = sc.nextInt();
+			} catch (Exception e) {
+				System.out.println("Veuillez entrer un caratère numérique !");
+			}
+			
+			switch(choix){
+			case 0 :
+				System.out.println("Retour au menu précédent.");
+				break;
+			case 1 :
+				m.salleDispo(salle);
+				break;
+			case 2 :
+				m.afficherFileAttente();
+				break;
+			case 3 :
+				m.sauvegarderVisites();
+				break;
+			case 4 :
+				m.afficherListeVisites();
+				break;
+			default :
+				System.out.println("Veuillez entrer un chiffre correspondant à votre choix !");
+				break;
+			}
 		}
 	}
 
